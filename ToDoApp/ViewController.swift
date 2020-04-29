e0e3c519070918d71f4c00d566fd8eb5ee4a2979
Sergei Demianenko <siriysgrey@gmail.com>
2020-04-29 20:04:01 +0300

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Serhii Demianenko on 29.04.2020.
+//  Copyright © 2020 Serhii Demianenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
